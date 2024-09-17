ea2adc4eb82743daeed62e031665876e768badab
AMayfet <amayfet@alfabank.ru>
2024-09-17 20:38:47 +0300

    main

--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,5 @@ export const FEATURE_1 = 'FEATURE_1'
 export const FEATURE_3 = 'FEATURE_3'
 export const FEATURE_4 = 'FEATURE_3'
 export const FEATURE_5 = 'FEATURE_5'
+
+export const FEATURE_6 = 'FEATURE_6'
